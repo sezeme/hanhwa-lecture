7e33bad5449ea2322876c4e06bf52eeacb5c11ef
sezeme <stellaa223@gmail.com>
2025-04-18 14:26:18 +0900

    commit

---
+++ 12_javascript/01_javascript-core/01_variable/01_data-type/04_undefined-and-null.js
@@ -0,0 +1,7 @@
+var undef/* = undefined (명시적 작성은 권장x) */; // 자바스크립트 엔진이 변수 초기화 시 undefined 사용
+console.log(undef);
+
+// null은 변수에 값이 없다는 것을 의도적으로 명시할 때 사용한다.
+var nullVar = 'something';
+nullVar = null;
+console.log(nullVar);
