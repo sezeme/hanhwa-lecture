b678ee21fd5bbf0e84a361b0f056e042311498a8
sezeme <stellaa223@gmail.com>
2025-04-18 12:41:05 +0900

    commit

---
+++ 12_javascript/01_javascript-core/01_variable/01_data-type/01_number.js
@@ -0,0 +1,12 @@
+/* 숫자 타입 */
+var integer = 10;
+var double = 5.5;
+var negative = -10;
+
+console.log(integer);
+console.log(double);
+console.log(negative);
+// 숫자 타입은 모두 실수로 처리 된다.
+console.log(integer == 10.0);
+console.log(integer / 4);
+console.log(integer % 4);
